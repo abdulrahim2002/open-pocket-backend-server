8321ce8b8b68463037703d8aabe3c3f4e8c4c1e5
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-15 20:13:44 +0530

    Setup fastify
    - install and save fastify as dependency
    - test fastify by running some basic code

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import Fastify from "fastify";
+
+const app = Fastify();
+
+app.get("/", async (request, reply) => {
+    return "Server is live!";
+});
+
+app.listen({port:7860, host: "0.0.0.0"}, (err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log("Server is running on http://localhost:7860");
+});
