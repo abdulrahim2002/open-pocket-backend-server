6374df4b02f4cef0e8d0fea4e4fe58c55638e71d
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-17 21:56:46 +0530

    Implement skeleton for `/send` endpoint
    - Implemented the basic request response structure for `/send` endpoint
    - The implementation is neither correct nor accurate currently
    - The documentation for `/send` endpoint is available here[1]
    
    [1]: https://abdulrahim2002.github.io/open-pocket-backend-server/docs/API-spec/send/

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { error } from "console";
 import Fastify, { type FastifySchema } from "fastify";
+import { url } from "inspector";
+import { title } from "process";
 
 const app = Fastify({
     logger: true,
@@ -241,6 +243,82 @@ app.post("/get", { schema: getRequestSchema }, async (request, reply) => {
         }
     }
 });
+
+/**
+ * /send endpoint
+ * https://abdulrahim2002.github.io/open-pocket-backend-server/docs/API-spec/send
+ **/
+
+const sendRequestSchema = {
+    // request body
+    body: {
+        type: 'object',
+        properties: {
+            consumer_key: { type: 'string' },
+            access_token: { type: 'string' },
+            actions: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        action: {
+                            type: 'string',
+                            enum: [ "add", "archive", "readd", "favorite", "unfavorite", "delete",
+                                    "tags_add", "tags_remove", "tags_replace", "tags_clear", "tag_rename", "tag_delete"
+                            ],
+                        },
+                        // This is actually inacurate. We need to define a different schema for each action
+                        item_id: { type: 'string', nullable: true },
+                        ref_id: { type: 'string', nullable: true },
+                        tags: { type: 'string', nullable: true },
+                        time: { type: 'string', format: 'date-time', nullable: true },
+                        title: { type: 'string', nullable: true },
+                        url: { type: 'string', format: 'uri', nullable: true },
+                        old_tag: { type: 'string', nullable: true },
+                        new_tag: { type: 'string', nullable: true },
+                    }
+                }
+            }
+        }
+    },
+    // request headers
+    headers: {
+        type: 'object',
+        properties: {
+            'Content-Type': { type: 'string', enum: ['application/json'] },
+        },
+        required: ['Content-Type']
+    },
+    // response schema
+    response: {
+        default: {
+            type: 'object',
+            properties: {
+                error: { type: 'boolean', default: true },
+            }
+        },
+        '2xx': {
+            type: 'object',
+            properties: {
+                status: { type: 'integer', default: 1 },
+                action_results: {
+                    type: 'array',
+                    items: { type: 'boolean' },
+                }
+            }
+        }
+    }
+};
+
+app.post("/send", { schema: sendRequestSchema }, async (request, reply) => {
+    // send a dummy response
+    return {
+        status: 1,
+        action_results: [true, false, true],
+    };
+});
+
+
 
 /* Health Check endpoint */
 app.get("/", async (request, reply) => {
