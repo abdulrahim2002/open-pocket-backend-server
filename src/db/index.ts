64b15242853a8764d6cc335aa16626b3a87184fb
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-27 01:30:09 +0530

    chore: Update drizzle database connector
    - Make it use mainConfig.DATABASE_URL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,6 @@
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
+import mainConfig from "@src/configs/main.config.js";
 
-if ( !process.env.DATABASE_URL ) {
-    throw new Error('DATABASE_URL is not defined');
-}
-
-const db: NodePgDatabase = drizzle(process.env.DATABASE_URL!);
+const db: NodePgDatabase = drizzle(mainConfig.DATABASE_URL);
 
 export default db;
