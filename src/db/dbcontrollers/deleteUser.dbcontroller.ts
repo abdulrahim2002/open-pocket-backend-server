1a57dd96c205577fd7d6aa6ff50b57edb8949a52
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-28 01:06:02 +0530

    feat: add deleteUser.dbcontroller API
    - Provide functionaluty to delete a user by their user id
    
    TODO: current bugs:
    - When the uid does not exist, we are returned an "Unknown Failure"
    - I don;t think so this should be the case, we should atleast know that
      the user we tried to delete does not exist

---
+++ src/db/dbcontrollers/deleteUser.dbcontroller.ts
@@ -0,0 +1,62 @@
+import db   from "@src/db/index.js";
+import { usersSchema } from "@src/db/schemas/users.schema.js";
+import { DrizzleQueryError, eq } from "drizzle-orm";
+import IDbControllerResponse, { OPSTATUS } from "./IDbControllerResponse.js";
+import { StatusCodes } from "http-status-codes";
+import { DatabaseError } from "pg-protocol";
+
+type userShape = typeof usersSchema.$inferSelect;
+
+async function deleteUser(uid: number): Promise<IDbControllerResponse<userShape>> {
+    try {
+        const deletedUser = await db.delete(usersSchema)
+                                    .where( eq(usersSchema.uid, uid) ).returning();
+        
+        if ( deletedUser[0] === undefined ) {
+            throw new Error("Unidentified Failure");
+        }
+
+        return {
+            success: true,
+            status: OPSTATUS.SUCCESS,
+            message: `Deleted User with uid: ${uid} successfully`,
+            recommendedHttpResponseCode: StatusCodes.OK,
+            data: deletedUser[0],
+        }
+    }
+    catch (error: any) {
+        if ( !(error instanceof DrizzleQueryError) || !(error.cause instanceof DatabaseError) ) {
+            return {
+                success: false,
+                status: OPSTATUS.UNKNOWN_FAILURE,
+                message: "Unknown Failure",
+            }
+        }
+
+        let     recommendedHttpResponseCode: number|undefined = undefined,
+                message = "",
+                originalError: DatabaseError = error.cause,
+                originalErrorCode: number = Number(originalError.code);
+        
+        switch (originalErrorCode) {
+            case OPSTATUS.CONNECTION_DOES_NOT_EXIST: {
+                recommendedHttpResponseCode = StatusCodes.SERVICE_UNAVAILABLE;
+                message = "Database Server is unreachable at the time";
+                break;
+            }
+            case OPSTATUS.CONNECTION_FAILURE: {
+                recommendedHttpResponseCode = StatusCodes.SERVICE_UNAVAILABLE;
+                message = "Database Server is unreachable at the time, try again later";
+            }
+        }
+
+        return {
+            success: false,
+            status: originalErrorCode,
+            message: message,
+            recommendedHttpResponseCode: recommendedHttpResponseCode,
+        }
+    }
+}
+
+export default deleteUser;
