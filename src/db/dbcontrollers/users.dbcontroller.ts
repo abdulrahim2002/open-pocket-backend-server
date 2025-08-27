beea19e63c4c777ef8afb67f31c354b9aecbd346
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-27 21:18:23 +0530

    Add users table in database schema
    - Add `users` table into the schema as `usersSchema`
    - Implement the `createUser` operation. Later, full CRUD operation will
      be implmented
    
    - Incorporate `http-status-codes` library into the project. It provides
    better way of handling http response codes from REST api's. And avoids
    the use of magic numbers like: `res.status(200)`
    Instead: `res.status(StatusCodes.OK)`
    
    - Define interface for db controller result. This will also incorporate
      a `recommendedHttpStatusCode` flag, which is based on the type of
      error encounterd while doing the operation on the database. This is
      increadibly useful for situations in which the error originates from
      Database layer, and relavent response code is required to be sent

---
+++ src/db/dbcontrollers/users.dbcontroller.ts
@@ -0,0 +1,69 @@
+import db              from "@src/db/index.js";
+import app             from "@src/app.js";
+import { usersSchema } from "@src/db/schemas/users.schema.js";
+import { StatusCodes } from "http-status-codes";
+import { DrizzleQueryError } from "drizzle-orm";
+import { DatabaseError }     from "pg-protocol";
+import IDbControllerResponse, { OPSTATUS } from "@src/db/dbcontrollers/dbcontroller.interface.js";
+
+type UserInsertShape = typeof usersSchema.$inferInsert;
+type UserShape       = typeof usersSchema.$inferSelect;
+
+export async function createUser(user: UserInsertShape): Promise<IDbControllerResponse<UserShape>> {
+
+    try {
+        const insertedUser = await db.insert(usersSchema).values(user).returning();
+
+        if ( insertedUser[0] === undefined ) {
+            throw new Error("Unknown Error"); // handled below
+        }
+
+        app.log.info(`User created successfully with uid: ${insertedUser[0].uid}`);
+
+        return {
+            success: true,
+            status: OPSTATUS.SUCCESS,
+            message: "User created successfully",
+            recommendedHttpResponseCode: StatusCodes.CREATED,
+            data: insertedUser[0],
+        }
+    }
+
+    catch (err: any) {
+
+        // if error has not originated from pg driver, no useful information can be extracted
+        if ( !(err instanceof DrizzleQueryError) || !(err.cause instanceof DatabaseError) ) {
+            return {
+                success: false,
+                status: OPSTATUS.UNKNOWN_FAILURE,
+            }
+        }
+
+        let recommendedHttpResponseCode: number|undefined = undefined,
+            message = "",
+            originalError: DatabaseError = err.cause,
+            errCode: number = Number(originalError.code);
+        
+        // try to find out what went wrong?
+        switch (errCode) {
+
+            case OPSTATUS.UNIQUE_VIOLATION: {
+                recommendedHttpResponseCode = StatusCodes.CONFLICT;
+                message = "User already exists";
+                break;
+            }
+            case OPSTATUS.CONNECTION_FAILURE: {
+                recommendedHttpResponseCode: StatusCodes.INTERNAL_SERVER_ERROR;
+                message: "Broken connection to database server";
+                break;
+            }
+        }
+
+        return {
+            success: false,
+            status: errCode,
+            recommendedHttpResponseCode: recommendedHttpResponseCode || StatusCodes.INTERNAL_SERVER_ERROR,
+            message: message || "Unknown Failure",
+        }
+    }
+}
---
+++ src/db/schemas/users.schema.ts
@@ -0,0 +1,18 @@
+/**
+ * Define schema for `users` table.
+ * See: https://abdulrahim2002.github.io/open-pocket-backend-server/docs/Database-Layer/database-schema/#users
+ **/
+import * as dpg from "drizzle-orm/pg-core";
+
+export const usersSchema = dpg.pgTable(
+    "users",
+    {
+        uid:        dpg.serial().primaryKey(),
+        provider:   dpg.text().notNull().default("open-pocket"),
+        name:       dpg.text().notNull(),
+        email:      dpg.text().unique(),
+    },
+    (table) => [
+        dpg.index("email_index").on(table.email),
+    ]    
+);
