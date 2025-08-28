b10d3ea7a681679e2ffda3851c7fb3beb49440df
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-28 17:05:14 +0530

    fix: turn of fileParallelism in vitest
    - by default, vite tries to run tests in different files in parallel
    - it does this by firing up multiple instances of fastify app, But the
      problem is that they all try to run on the same port
    - therefore some instances (hence tests) fail with ADDRESS ALREADY IN
      USE error
    
    - to fix this I've temporarilly turned of fileParallelism, but we need a
      better solution in the long term

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,15 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+
   test: {
     globals: true,
     environment: 'node',
     include: ["src/tests/**/*.test.ts"],
-  },
-})
+    // vite tries to fire up multiple instances of fastify app
+    // which try to run on same port resulting in error.
+    // TODO: find a better fix than turing file parallelism off
+    fileParallelism: false,
+  }
+
+});
