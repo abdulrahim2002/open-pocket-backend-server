ff30eb6ce9e5dd2c4abe2a7c99b57740ddf0db79
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-28 23:46:53 +0530

    feat: create index on `tag`
    - create index on tag to quickly search all articles with a particular
      tag

--- src/db/schemas/tags.schema.ts
+++ src/db/schemas/tags.schema.ts
@@ -2,7 +2,7 @@
  * Define schema for `users` table.
  * See: https://abdulrahim2002.github.io/open-pocket-backend-server/docs/Database-Layer/database-schema/#tagging
  **/
-import { pgTable, serial, text, primaryKey, foreignKey } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, primaryKey, foreignKey, index } from "drizzle-orm/pg-core";
 import { usersSchema } from "@src/db/schemas/users.schema.js";
 
 export const tagsSchema = pgTable(
@@ -17,6 +17,7 @@ export const tagsSchema = pgTable(
         foreignKey({
             columns: [ tagsSchema.user_id ],
             foreignColumns: [ usersSchema.user_id ]
-        })
+        }),
+        index().on(table.tag),
     ]
 );
