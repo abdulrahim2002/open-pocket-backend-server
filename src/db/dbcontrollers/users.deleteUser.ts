09e5ffb9f47a062a488445a9392b68efce4fed88
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-29 17:17:28 +0530

    feat: add error hounder to deleteUser dbcontroller

--- src/db/dbcontrollers/users.deleteUser.ts
+++ src/db/dbcontrollers/users.deleteUser.ts
@@ -1,10 +1,11 @@
 import db               from "@src/db/index.js";
 import { usersSchema }  from "@src/db/schemas/users.schema.js";
-import { DrizzleQueryError, eq } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import IDbControllerResponse, { OPSTATUS }
     from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
 import { StatusCodes }      from "http-status-codes";
-import { DatabaseError }    from "pg-protocol";
+import app from "@src/app.js";
+import { houndError } from "./commons/errorHounder.js";
 
 type userShape = typeof usersSchema.$inferSelect;
 
@@ -28,37 +29,8 @@ async function deleteUser(user_id: number): Promise<IDbControllerResponse<userSh
         }
     }
     catch (error: any) {
-        if ( !(error instanceof DrizzleQueryError) || !(error.cause instanceof DatabaseError) ) {
-            return {
-                success: false,
-                status: OPSTATUS.UNKNOWN_FAILURE,
-                message: "Unknown Failure",
-            }
-        }
-
-        let     recommendedHttpResponseCode: number|undefined = undefined,
-                message = "",
-                originalError: DatabaseError = error.cause,
-                originalErrorCode: number = Number(originalError.code);
-
-        switch (originalErrorCode) {
-            case OPSTATUS.CONNECTION_DOES_NOT_EXIST: {
-                recommendedHttpResponseCode = StatusCodes.SERVICE_UNAVAILABLE;
-                message = "Database Server is unreachable at the time";
-                break;
-            }
-            case OPSTATUS.CONNECTION_FAILURE: {
-                recommendedHttpResponseCode = StatusCodes.SERVICE_UNAVAILABLE;
-                message = "Database Server is unreachable at the time, try again later";
-            }
-        }
-
-        return {
-            success: false,
-            status: originalErrorCode,
-            message: message,
-            recommendedHttpResponseCode: recommendedHttpResponseCode,
-        }
+        app.log.error(error);
+        return houndError(error);
     }
 }
 
