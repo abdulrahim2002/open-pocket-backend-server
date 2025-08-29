5fa8ac665af975a2ff4ffe68ee0bdb830f42dead
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-29 17:13:32 +0530

    feat: add errorHounder to tags.createTag API

--- src/db/dbcontrollers/tags.createTag.ts
+++ src/db/dbcontrollers/tags.createTag.ts
@@ -2,10 +2,9 @@ import db              from "@src/db/index.js";
 import app             from "@src/app.js";
 import { tagsSchema }  from "@src/db/schemas/tags.schema.js";
 import { StatusCodes } from "http-status-codes";
+import { houndError }  from "./commons/errorHounder.js";
 import IDbControllerResponse, { OPSTATUS }
                        from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
-import { DrizzleQueryError } from "drizzle-orm";
-import { DatabaseError }     from "pg-protocol";
 
 type tagInsertShape = typeof tagsSchema.$inferInsert;
 type tagShape       = typeof tagsSchema.$inferSelect;
@@ -28,45 +27,9 @@ async function createTag(tag: tagInsertShape): Promise<IDbControllerResponse<tag
         }
     }
 
-    catch (error) {
+    catch (error: any) {
         app.log.error(error);
-
-        if ( !(error instanceof DrizzleQueryError) || !(error.cause instanceof DatabaseError) ) {
-            return {
-                success: false,
-                status: OPSTATUS.UNKNOWN_FAILURE,
-                message: "Unknown Failure",
-            }
-        }
-
-        let     recommendedHttpResponseCode: number|undefined = undefined,
-                message = "",
-                originalErrorCode = Number(error.cause.code);
-
-        switch (originalErrorCode) {
-            case OPSTATUS.FOREIGN_KEY_VIOLATION: {
-                recommendedHttpResponseCode = StatusCodes.BAD_REQUEST;
-                message = "You probably tried to insert a tag with non existant user_id or article_id";
-                break;
-            }
-            case OPSTATUS.CONNECTION_FAILURE: {
-                recommendedHttpResponseCode = StatusCodes.SERVICE_UNAVAILABLE;
-                message = "Database Server is probably down. Please try again later";
-                break;
-            }
-            case OPSTATUS.UNIQUE_VIOLATION: {
-                recommendedHttpResponseCode=  StatusCodes.CONFLICT;
-                message = "You probably tried inserting a duplicate record";
-                break;
-            }
-        }
-
-        return {
-            success: false,
-            status: originalErrorCode,
-            message: message,
-            recommendedHttpResponseCode: recommendedHttpResponseCode,
-        }
+        return houndError(error);
     }
 }
 
