bf81c3a0204b55d52fb6bc7fd132d13c91d6472d
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-30 01:46:36 +0530

    feat: add `deleteTag` dbcontroller
    - the API deletes a tag using it's `tag_id`

---
+++ src/db/dbcontrollers/tags.deleteTag.ts
@@ -0,0 +1,35 @@
+import app              from "@src/app.js";
+import db               from "@src/db/index.js";
+import { tagsSchema }   from "@src/db/schemas/tags.schema.js";
+import { StatusCodes }  from "http-status-codes";
+import { eq }           from "drizzle-orm";
+import { houndError }   from "./commons/errorHounder.js";
+import IDbControllerResponse, { OPSTATUS }
+                        from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
+
+type tagShape = typeof tagsSchema.$inferSelect;
+
+async function deleteTag(tag_id: number): Promise<IDbControllerResponse<tagShape>> {
+    try {
+        const deletedTags = await db.delete(tagsSchema)
+                                    .where(eq(tagsSchema.tag_id, tag_id)).returning();
+
+        if ( deletedTags[0] === undefined ) {
+            throw new Error("Unknown Failure"); // handled below
+        }
+
+        return {
+            success: true,
+            status: OPSTATUS.SUCCESS,
+            message: "tag deleted successfully",
+            recommendedHttpResponseCode: StatusCodes.OK,
+            data: deletedTags[0],
+        }
+    }
+    catch (error: any) {
+        app.log.error(error);
+        return houndError(error);
+    }
+}
+
+export default deleteTag;
