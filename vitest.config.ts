6b85a5b6d06ad7df2f582840fba23725502b16f7
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-29 17:24:40 +0530

    fix: indent using 4 spaces

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vitest/config';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
 
   test: {
     globals: true,
-    environment: 'node',
+    environment: "node",
     include: ["src/tests/**/*.test.ts"],
     // vite tries to fire up multiple instances of fastify app
     // which try to run on same port resulting in error.
     // TODO: find a better fix than turing file parallelism off
     // https://vitest.dev/guide/parallelism.html#file-parallelism
     fileParallelism: false,
-  }
-
+  },
 });
