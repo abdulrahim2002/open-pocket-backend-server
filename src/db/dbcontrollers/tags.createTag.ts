c956cc80af4e7769acd3e80ab432ab5d4ed45e47
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-30 01:36:36 +0530

    chore: relative import -> path aliased import

--- src/db/dbcontrollers/tags.createTag.ts
+++ src/db/dbcontrollers/tags.createTag.ts
@@ -2,7 +2,7 @@ import db              from "@src/db/index.js";
 import app             from "@src/app.js";
 import { tagsSchema }  from "@src/db/schemas/tags.schema.js";
 import { StatusCodes } from "http-status-codes";
-import { houndError }  from "./commons/errorHounder.js";
+import { houndError }  from "@src/db/dbcontrollers/commons/errorHounder.js";
 import IDbControllerResponse, { OPSTATUS }
                        from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
 
