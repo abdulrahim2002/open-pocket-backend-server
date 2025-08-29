cdc3a08ea08b20b25462e9ca387fffc4a8a0ad3f
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-29 17:02:15 +0530

    feat:use houndError in createUser API
    - use the hound error to investigate the error

--- src/db/dbcontrollers/users.createUser.ts
+++ src/db/dbcontrollers/users.createUser.ts
@@ -2,10 +2,9 @@ import db              from "@src/db/index.js";
 import app             from "@src/app.js";
 import { usersSchema } from "@src/db/schemas/users.schema.js";
 import { StatusCodes } from "http-status-codes";
-import { DrizzleQueryError } from "drizzle-orm";
-// TODO: Remove dependency on pg-protocol since it is not mentioned in package.json
-import { DatabaseError }     from "pg-protocol";
-import IDbControllerResponse, { OPSTATUS } from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
+import { houndError }  from "./commons/errorHounder.js";
+import IDbControllerResponse, { OPSTATUS }
+        from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
 
 type UserInsertShape = typeof usersSchema.$inferInsert;
 type UserShape       = typeof usersSchema.$inferSelect;
@@ -34,41 +33,9 @@ async function createUser(user: UserInsertShape): Promise<IDbControllerResponse<
 
         app.log.error(err);
 
-        // if error has not originated from pg driver, no useful information can be extracted
-        if ( !(err instanceof DrizzleQueryError) || !(err.cause instanceof DatabaseError) ) {
-            return {
-                success: false,
-                status: OPSTATUS.UNKNOWN_FAILURE,
-                message: "Unknown Failure"
-            }
-        }
+        const errorDetails = houndError(err);
 
-        let recommendedHttpResponseCode: number|undefined = undefined,
-            message = "",
-            originalError: DatabaseError = err.cause,
-            errCode: number = Number(originalError.code);
-
-        // try to find out what went wrong?
-        switch (errCode) {
-
-            case OPSTATUS.UNIQUE_VIOLATION: {
-                recommendedHttpResponseCode = StatusCodes.CONFLICT;
-                message = "User already exists";
-                break;
-            }
-            case OPSTATUS.CONNECTION_FAILURE: {
-                recommendedHttpResponseCode = StatusCodes.SERVICE_UNAVAILABLE;
-                message = "Broken connection to database server";
-                break;
-            }
-        }
-
-        return {
-            success: false,
-            status: errCode,
-            recommendedHttpResponseCode: recommendedHttpResponseCode,
-            message: message || "Unknown Failure",
-        }
+        return errorDetails;
     }
 }
 
