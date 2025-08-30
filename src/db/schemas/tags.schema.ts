d4cc0b40870e8ef669b393530493ebcfbe3b48c9
abdul <cseabdulrahim@gmail.com>
2025-08-30 22:14:00 +0530

    fix: the item_id needs to be bigint
    - the item_id needs to be bigint as per specification[1]
    - since, this field is foreign key, it should comply with corresponding
      field in articles table
    
    [1]: https://abdulrahim2002.github.io/open-pocket-backend-server/docs/Database-Layer/database-schema/#articles

--- src/db/schemas/tags.schema.ts
+++ src/db/schemas/tags.schema.ts
@@ -2,15 +2,16 @@
  * Define schema for `users` table.
  * See: https://abdulrahim2002.github.io/open-pocket-backend-server/docs/Database-Layer/database-schema/#tags
  **/
-import { integer, pgTable, text, foreignKey, index, unique } from "drizzle-orm/pg-core";
 import usersSchema  from "@src/db/schemas/users.schema.js";
+import {    integer, bigint, pgTable, text,
+            foreignKey, index, unique } from "drizzle-orm/pg-core";
 
 const tagsSchema = pgTable(
     "tags",
     {
         tag_id:         integer().primaryKey().generatedAlwaysAsIdentity(),
         user_id:        integer().notNull(),
-        item_id:        integer().notNull(),
+        item_id:        bigint({mode: "bigint"}).notNull(),
         tag_name:       text().notNull(),
     },
     (table): any[] => [
