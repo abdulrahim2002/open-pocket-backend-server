ba03cea2aa0ebb5c862dc3fda3ff14c72a8c3e85
Abdul Rahim <abdul.rahim@myyahoo.com>
2025-08-30 17:03:07 +0530

    fix: test for `createTag` now cleans up stale data
    - previously, we were trying to
    
    	- create sample user
    	- create sample tag
    	- delete sample user   ----- 	ERROR -> foreign key constraint
    	  				violation. since sample tag
    					still has user_id of sample user
    
    - Now we have fixed the order.
    
    	- create sample user
    	- create sample tag
    	- delete sample tag
    	- delete sample user

--- src/tests/dbcontrollers/tags.createTag.test.ts
+++ src/tests/dbcontrollers/tags.createTag.test.ts
@@ -5,34 +5,46 @@ import createUser       from "@src/db/dbcontrollers/users.createUser.js";
 import deleteUser from "@src/db/dbcontrollers/users.deleteUser.js";
 import { OPSTATUS }     from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
 import { StatusCodes }  from "http-status-codes";
+import deleteTag from "@src/db/dbcontrollers/tags.deleteTag.js";
 
 
 const testUserForCreateTag: typeof usersSchema.$inferInsert = {
     provider:   "vitest",
-    name:       "test-user-for-create-tag",
-    email:      "test-user-for-create-tag@mail.com",
+    name:       "testUser-createTag",
+    email:      "testUser-createTag@mail.com",
 };
 
 test("createTag", async () => {
-    // first create a test user
-    const res1 = await createUser(testUserForCreateTag);
-    if (!res1.success) {
+    // step 1: create a test user
+    const resCreateUser = await createUser(testUserForCreateTag);
+    if (!resCreateUser.success) {
         throw new Error("Precondition Failure: Couldn't create a user in the first place");
     }
 
-    // create a tag with user_id = user_id of newly created user
+    // create a tag with user_id=user_id of newly created user
     const testTagForCreateTag: typeof tagsSchema.$inferInsert =  {
         tag_name: "vitest",
-        user_id: res1.data!.user_id, // assuming it is present here
+        user_id: resCreateUser.data!.user_id,
         article_id: 123,
     };
 
-    const res2 = await createTag(testTagForCreateTag);
+    const resCreateTag = await createTag(testTagForCreateTag);
 
-    // reverse the operations -> delete user and tag
-    await deleteUser(res1.data!.user_id);
 
-    expect(res2).toMatchObject({
+    // delete the newly created tag. We only need the resCreateTag.data
+    // so it is safe to delete.
+    const resDelTag = await deleteTag(resCreateTag.data!.tag_id);
+    if (!resDelTag.success) {
+        throw new Error("We could not delete the created tag.");
+    }
+
+    // delete the created tag, to reverse operations
+    const resDelUser = await deleteUser(resCreateUser.data!.user_id);
+    if (!resDelUser) {
+        throw new Error("We could not delete the test user.");
+    }
+
+    expect(resCreateTag).toMatchObject({
         success: true,
         status: OPSTATUS.SUCCESS,
         message: "tag create successfully",
