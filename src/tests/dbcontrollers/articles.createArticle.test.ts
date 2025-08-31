3d09b4a58b78d77b625c634e0e43400915e78d69
abdul <cseabdulrahim@gmail.com>
2025-08-31 13:42:08 +0530

    feat: add test for `createArticle` API
    - add the test for checking article creation functionality

---
+++ src/tests/dbcontrollers/articles.createArticle.test.ts
@@ -0,0 +1,38 @@
+import createArticle    from "@src/db/dbcontrollers/articles.createArticle.js";
+import articlesSchema   from "@src/db/schemas/articles.schema.js";
+import { OPSTATUS }     from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
+import { StatusCodes }  from "http-status-codes";
+
+
+const testArticleForCreateArticle: typeof articlesSchema.$inferInsert = {
+    user_id: 123, // TODO: add foreign key constraint
+    status: 0,
+    favorite: false,
+    resolved_title: "Sample Resolved Title",
+    resolved_url: "https://sample-resolved-url.com/",
+    excerpt: "sample excerpt describing the item in detail",
+    is_article: true,
+    is_index: false,
+    has_video: false,
+    has_image: false,
+    word_count: 123,
+    time_added: new Date(),
+    time_updated: new Date(),
+    top_image_url: "https://topimage.io/image.png/",
+    author_name: "sample-author",
+};
+
+
+test("createArticle", async () => {
+    const resCreateArticle = await createArticle(testArticleForCreateArticle);
+
+    // TODO: delete the article to reverse changes made
+
+    expect(resCreateArticle).toMatchObject({
+        success: true,
+        status: OPSTATUS.SUCCESS,
+        message: "article created successfully",
+        recommendedHttpResponseCode: StatusCodes.CREATED,
+        data: testArticleForCreateArticle,
+    });
+});
