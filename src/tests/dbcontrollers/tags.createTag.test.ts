dc48bd5d772968a181ea3acf633eb213829e2aa8
abdul <cseabdulrahim@gmail.com>
2025-08-31 15:09:18 +0530

    fix: update createTag test according to FK's
    - update createTag test according to foreign key constraint
    - the article.item_id is a foreign key constraint now, so we cannot
      insert a rantdome item_id into the database. until it is present in
      the articles table

--- src/tests/dbcontrollers/tags.createTag.test.ts
+++ src/tests/dbcontrollers/tags.createTag.test.ts
@@ -6,12 +6,32 @@ import deleteUser       from "@src/db/dbcontrollers/users.deleteUser.js";
 import { OPSTATUS }     from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
 import { StatusCodes }  from "http-status-codes";
 import deleteTag        from "@src/db/dbcontrollers/tags.deleteTag.js";
-
+import articlesSchema   from "@src/db/schemas/articles.schema.js";
+import createArticle    from "@src/db/dbcontrollers/articles.createArticle.js";
+import deleteArticle    from "@src/db/dbcontrollers/articles.deleteArticle.js";
 
 const testUserForCreateTag: typeof usersSchema.$inferInsert = {
     provider:   "vitest",
     name:       "testUser-createTag",
     email:      "testUser-createTag@mail.com",
+};
+
+const testArticleForCreateTag: typeof articlesSchema.$inferInsert = {
+    user_id: 123,
+    status: 0,
+    favorite: false,
+    resolved_title: "Sample Resolved Title",
+    resolved_url: "https://sample-resolved-url.com/",
+    excerpt: "sample excerpt describing the item in detail",
+    is_article: true,
+    is_index: false,
+    has_video: 0,
+    has_image: 0,
+    word_count: 123,
+    time_added: new Date(),
+    time_updated: new Date(),
+    top_image_url: "https://topimage.io/image.png/",
+    author_name: "sample-author",
 };
 
 test("createTag", async () => {
@@ -21,11 +41,17 @@ test("createTag", async () => {
         throw new Error("Precondition Failure: Couldn't create a user in the first place");
     }
 
+    // create a test article
+    const resCreateArticle = await createArticle(testArticleForCreateTag);
+    if (!resCreateArticle.success) {
+        throw new Error("Precondition Failure: Couldn't create related article in the first place");
+    }
+
     // create a tag with user_id=user_id of newly created user
     const testTagForCreateTag: typeof tagsSchema.$inferInsert =  {
         tag_name: "vitest",
         user_id: resCreateUser.data!.user_id,
-        item_id: BigInt(12312312),
+        item_id: resCreateArticle.data!.item_id,
     };
 
     const resCreateTag = await createTag(testTagForCreateTag);
@@ -42,6 +68,12 @@ test("createTag", async () => {
     const resDelUser = await deleteUser(resCreateUser.data!.user_id);
     if (!resDelUser) {
         throw new Error("We could not delete the test user.");
+    }
+
+    // delete the newly created article
+    const resDelArticle = await deleteArticle(resCreateArticle.data!.item_id);
+    if (!resDelArticle) {
+        throw new Error("We could not delete the created article.");
     }
 
     expect(resCreateTag).toMatchObject({
