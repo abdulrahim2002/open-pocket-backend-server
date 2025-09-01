97405551eb2fb91cc1ed7581225285970f16cda2
abdul <cseabdulrahim@gmail.com>
2025-09-01 23:57:26 +0530

    chore: move sendRequestSchema into schemas dir

--- src/routes/send.ts
+++ src/routes/send.ts
@@ -1,71 +1,10 @@
-import { FastifySchema, type FastifyInstance } from "fastify";
+import sendRequestSchema from "@src/routes/schemas/send.schema.js";
+import { type FastifyInstance } from "fastify";
 
 /**
  * /send endpoint
  * https://abdulrahim2002.github.io/open-pocket-backend-server/docs/API-spec/send
  **/
-
-const sendRequestSchema: FastifySchema = {
-    // request body
-    body: {
-        type: 'object',
-        properties: {
-            consumer_key: { type: 'string' },
-            access_token: { type: 'string' },
-            actions: {
-                type: 'array',
-                items: {
-                    type: 'object',
-                    properties: {
-                        action: {
-                            type: 'string',
-                            enum: [ "add", "archive", "readd", "favorite", "unfavorite", "delete",
-                                    "tags_add", "tags_remove", "tags_replace", "tags_clear", "tag_rename", "tag_delete"
-                            ],
-                        },
-                        // This is actually inacurate. We need to define a different schema for each action
-                        item_id: { type: 'string', nullable: true },
-                        ref_id: { type: 'string', nullable: true },
-                        tags: { type: 'string', nullable: true },
-                        time: { type: 'string', format: 'date-time', nullable: true },
-                        title: { type: 'string', nullable: true },
-                        url: { type: 'string', format: 'uri', nullable: true },
-                        old_tag: { type: 'string', nullable: true },
-                        new_tag: { type: 'string', nullable: true },
-                    }
-                }
-            }
-        }
-    },
-    // request headers
-    headers: {
-        type: 'object',
-        properties: {
-            'Content-Type': { type: 'string', enum: ['application/json'] },
-        },
-        required: ['Content-Type']
-    },
-    // response schema
-    response: {
-        default: {
-            type: 'object',
-            properties: {
-                error: { type: 'boolean', default: true },
-            }
-        },
-        '2xx': {
-            type: 'object',
-            properties: {
-                status: { type: 'integer', default: 1 },
-                action_results: {
-                    type: 'array',
-                    items: { type: 'boolean' },
-                }
-            }
-        }
-    }
-};
-
 async function sendEndpoint( app: FastifyInstance ) {
     app.post("/send", { schema: sendRequestSchema }, async (request, reply) => {
         // send a dummy response
