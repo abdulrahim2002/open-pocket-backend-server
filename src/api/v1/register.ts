fbef4cbb918db491f7bb77fd8ddae520a2c9a3f9
abdul <cseabdulrahim@gmail.com>
2025-09-02 20:04:50 +0530

    fix: strip down the register API and style fixes
    - remove the functionality of register API, we will reimplement it
      cleanly

--- src/api/v1/register.ts
+++ src/api/v1/register.ts
@@ -1,40 +1,18 @@
+import registerEndpointContract from "@src/api/v1/contracts/register.contract.js";
 import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
-import bcrypt                       from "bcrypt";
-import createUser                   from "@src/db/dbcontrollers/users.createUser.js";
-import registerEndpointContract     from "./contracts/register.contract.js";
 
 const registerEndpoint: FastifyPluginAsyncJsonSchemaToTs = async function (app) {
+
     app.post(
         "/register",
         {schema: registerEndpointContract},
         async (request, reply) => {
-
-        const { name, email, password } = request.body;
-        app.log.info(`user received with name: ${name}, email: ${email}`);
-
-        // hash the password
-        const hashed_password: string = await bcrypt.hash(password, 8);
-
-        const resCreateUser = await createUser({
-            name: name,
-            email: email,
-            hashed_password: hashed_password,
-        });
-
-        if ( resCreateUser.success ) {
-            reply.statusCode = resCreateUser.recommendedHttpResponseCode || 200;
+            // return a dummy response
             return {
-                status: 1,
-                message: "Created Successfully",
+                status: 200,
             }
         }
-        else {
-            reply.statusCode = resCreateUser.recommendedHttpResponseCode || 400;
-            return {
-                error: true,
-            }
-        }
-    });
-}
+    );
+};
 
 export default registerEndpoint;
