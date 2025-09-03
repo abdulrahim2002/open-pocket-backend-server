29c3f0ce08be04a470d553212d89864ee6fff38f
abdul <cseabdulrahim@gmail.com>
2025-09-03 13:34:03 +0530

    feat: add loginEndpoint contract

---
+++ src/api/v1/contracts/login.contract.ts
@@ -0,0 +1,78 @@
+const registerEndpointContract = {
+    // request body
+    body: {
+        type: "object",
+        properties: {
+            email:      { type: "string" },
+            password:   { type: "string" },
+        },
+        required: ["email", "password"],
+    },
+    // request headers
+    headers: {
+        type: "object",
+        properties: {
+            "Content-Type": { type: "string", enum: ["application/json"] },
+        },
+        required: ["Content-Type"],
+    },
+    // response schema
+    response: {
+        // inspired by: https://docs.oasis-open.org/odata/odata-json-format/v4.0/errata02/os/odata-json-format-v4.0-errata02-os-complete.html#_Toc403940655
+        default: {
+            type: "object",
+            properties: {
+                error: {
+                    type: "object",
+                    properties: {
+                        code: { type: "integer" },
+                        message: { type: "string" },
+                        details: {
+                            type: "array",
+                            items: {
+                                type: "object",
+                                properties: {
+                                    code: { type: "number" },
+                                    message: { type: "string" }
+                                },
+                                required: [ "code", "message" ]
+                            }
+                        },
+                    },
+                    required: [ "code", "message" ]
+                }
+            },
+            required: [ "error" ]
+        },
+        "2xx": {
+            type: "object",
+            additionalProperties: false,
+            properties: {
+                user: {
+                    type: "object",
+                    additionalProperties: false,
+                    properties: {
+                        id:     { type: "string" },
+                        name:   { type: "string" },
+                        email:  { type: "string" },
+                        provider: { type: "string" },
+                    },
+                    required: [ "id", "email" ]
+                },
+                tokens: {
+                    type: "object",
+                    additionalProperties: false,
+                    properties: {
+                        accessToken:   { type: "string" },
+                        refreshToken:  { type: "string" },
+                        tokenType:     { type: "string", const: "Bearer" },
+                    },
+                    required: [ "accessToken", "refreshToken", "tokenType" ]
+                }
+            },
+            required: [ "user", "tokens" ]
+        }
+    },
+} as const; // this is important for type inference on `req.body`
+
+export default registerEndpointContract;
