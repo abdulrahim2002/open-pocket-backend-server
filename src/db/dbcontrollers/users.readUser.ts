1e4b21150ec6f858a59f235bb1fea6920367f698
abdul <cseabdulrahim@gmail.com>
2025-09-03 15:12:18 +0530

    refactor: add TODO for readUser API
    - we only nede one record here. And are unnecessarily dealing with
      multiple items

--- src/db/dbcontrollers/users.readUser.ts
+++ src/db/dbcontrollers/users.readUser.ts
@@ -19,6 +19,8 @@ async function readUser(identifier: number|string): Promise<IDbControllerRespons
         let foundUser;
 
         // find user by email or id depending on the input type
+        // TODO: use query to find only one record
+        // https://orm.drizzle.team/docs/rqb#find-first
         if ( typeof identifier === "number" ) {
             foundUser = await db.select().from(usersSchema)
                                         .where( eq(usersSchema.user_id, identifier) );
