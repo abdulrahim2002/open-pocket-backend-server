a7ae1e78a098575219e39b3fb4785213dcf492b8
abdul <cseabdulrahim@gmail.com>
2025-09-03 12:14:59 +0530

    feat: `/register` is functional now
    - implement the `/register` endpoint according to the contracts
    - the API is working perfectly. If the user tries to insert a duplicate
      record, for example, maybe the user already exists and still tries to
      insert a new record with the same email. then it will throw a 409
      (CONFLICT) status code.

--- src/api/v1/register.ts
+++ src/api/v1/register.ts
@@ -1,5 +1,10 @@
 import registerEndpointContract from "@src/api/v1/contracts/register.contract.js";
-import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import usersSchema              from "@src/db/schemas/users.schema.js";
+import createUser               from "@src/db/dbcontrollers/users.createUser.js";
+import bcrypt                   from "bcrypt";
+import { StatusCodes }          from "http-status-codes";
+import { FastifyPluginAsyncJsonSchemaToTs }
+                                from "@fastify/type-provider-json-schema-to-ts";
 
 const registerEndpoint: FastifyPluginAsyncJsonSchemaToTs = async function (app) {
 
@@ -7,11 +12,47 @@ const registerEndpoint: FastifyPluginAsyncJsonSchemaToTs = async function (app) 
         "/register",
         {schema: registerEndpointContract},
         async (request, reply) => {
-            // return a dummy response
+
+            const { name, email, password } = request.body;
+            const hashed_password = await bcrypt.hash(password, 3);
+
+            const newUser: typeof usersSchema.$inferInsert = {
+                name: name,
+                email: email,
+                hashed_password: hashed_password,
+            };
+
+            const resCreateUser = await createUser(newUser);
+
+            if (resCreateUser.success) {
+                reply.status(StatusCodes.CREATED);
+                return {
+                    data: {
+                        type: "users",
+                        id: String(resCreateUser.data!.user_id),
+                        attributes: {
+                            name: resCreateUser.data!.name,
+                            email: resCreateUser.data!.email
+                        }
+
+                    }
+                }
+            }
+
+            const   errorStatusCode = resCreateUser.recommendedHttpResponseCode
+                                        ?? StatusCodes.INTERNAL_SERVER_ERROR,
+                    message = resCreateUser.message
+                                ?? "Something Went Wrong, please try again later";
+
+            reply.status(errorStatusCode);
             return {
-                status: 200,
+                error: {
+                    code: errorStatusCode,
+                    message: message,
+                }
             }
         }
+
     );
 };
 
