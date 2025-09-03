5bbabdb9edf11a9db2594b398d9bce13118fc1b6
abdul <cseabdulrahim@gmail.com>
2025-09-03 15:33:24 +0530

    feat: add the login endpoint

---
+++ src/api/v1/login.ts
@@ -0,0 +1,62 @@
+import loginEndpointContract        from "@src/api/v1/contracts/login.contract.js";
+import readUser                     from "@src/db/dbcontrollers/users.readUser.js";
+import { StatusCodes }              from "http-status-codes";
+import bcrypt                       from "bcrypt";
+import { FastifyPluginAsyncJsonSchemaToTs }
+                                    from "@fastify/type-provider-json-schema-to-ts"
+
+const loginEndpoint: FastifyPluginAsyncJsonSchemaToTs = async (app) => {
+
+    app.post(
+        "/login",
+        {schema: loginEndpointContract},
+        async (request, response) => {
+
+            const { email, password } = request.body;
+
+            const resReadUser = await readUser(email);
+
+            if ( !resReadUser.success ) {
+                // user does not exist or something else went wrong
+                // TODO: segregate these 2 issues
+                response.status(StatusCodes.UNAUTHORIZED);
+                return {
+                    error: {
+                        code: StatusCodes.UNAUTHORIZED,
+                        message: "Invalid email or password!"
+                    }
+                }
+            }
+
+            const hashed_password = await bcrypt.hash(password, 3);
+            if ( hashed_password !== resReadUser.data!.hashed_password ) {
+                // incorrect password
+                response.status(StatusCodes.UNAUTHORIZED);
+                return {
+                    error: {
+                        code: StatusCodes.UNAUTHORIZED,
+                        message: "Invalid email or password!"
+                    }
+                }
+            }
+
+            // TODO: all good. issue tokens.
+            // for now just return the user
+            response.status(StatusCodes.OK);
+            return {
+                data: {
+                    type: "users",
+                    id: String(resReadUser.data!.user_id),
+                    attributes: {
+                        name: resReadUser.data!.name,
+                        email: resReadUser.data!.email
+                    }
+                }
+            }
+
+        }
+
+    );
+};
+
+export default loginEndpoint;
