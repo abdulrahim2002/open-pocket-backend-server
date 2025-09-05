883a39d5157e97d332f1f954e139ef36cd5c267b
abdul <cseabdulrahim@gmail.com>
2025-09-05 15:14:29 +0530

    fix: add the routes loader itself

---
+++ src/api/v1/index.ts
@@ -0,0 +1,20 @@
+import addEndpoint      from "@src/api/v1/add.js";
+import getEndpoint      from "@src/api/v1/get.js";
+import sendEndpoint     from "@src/api/v1/send.js";
+
+import loginEndpoint    from "@src/api/v1/login.js";
+import refreshEndpoint  from "@src/api/v1/refresh.js";
+import registerEndpoint from "@src/api/v1/register.js";
+
+import { FastifyInstance } from "fastify";
+
+async function routesLoader(app: FastifyInstance) {
+    app.register(addEndpoint);
+    app.register(getEndpoint);
+    app.register(sendEndpoint);
+    app.register(loginEndpoint);
+    app.register(refreshEndpoint);
+    app.register(registerEndpoint);
+}
+
+export default routesLoader;
