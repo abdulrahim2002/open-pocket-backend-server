68f37bab268ad07ce63cbc53ebd2f3e2bdcd997d
abdul <cseabdulrahim@gmail.com>
2025-09-06 18:22:12 +0530

    chore: add comment

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,12 @@ import Fastify          from "fastify";
 import mainConfig       from "@src/configs/main.config.js";
 import authentication   from "@src/commons/authentication.js";
 import routesLoaderV1   from "@src/api/v1/index.js";
-import redis            from "@src/commons/redis.js";
 
 const app = Fastify({
     logger: true,
 });
 
-
+// authentication infrastructure
 app.register(authentication);
 
 // register routes
@@ -26,7 +25,6 @@ app.listen(
     (err, addr) => {
         if (err) {
             app.log.error(err);
-            redis.destroy();
             process.exit(1);
         }
         app.log.debug(`Server is running on http://localhost:${addr}`);
