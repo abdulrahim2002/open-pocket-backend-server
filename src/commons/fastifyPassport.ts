c6a1d2bb9d0ec4d34860fe48f55e291856f5fcdd
abdul <cseabdulrahim@gmail.com>
2025-09-06 14:29:58 +0530

    fix: it is safe to use whole user in req.user
    - with the newly gained understanding that deserializer will populate
      the request.session.passport which in turn is stored at the client
      side. We now only store { user_id } on the client side. Hence, we only
      supply user_id as identifier
    
    - with the understanding that the validation function inside any
      strategy when returning done(null, user) at last (on success)
      populates request.user.
    
      and request.user is only available on the server, not client side. It
      is safe to have the whole user object on request.server

--- src/commons/fastifyPassport.ts
+++ src/commons/fastifyPassport.ts
@@ -7,24 +7,18 @@ import readUser             from "@src/db/dbcontrollers/users.readUser.js";
 import { Strategy as LocalStrategy }             from "passport-local";
 import { Strategy as JwtStrategy, ExtractJwt }   from "passport-jwt";
 
-const fastifyPassport = new Authenticator();
+const fastifyPassport = new Authenticator({
+    // clearSessionIgnoreFields: add fields you do not want to be cleared in request.session
+});
 
-// this function shall return a user_id, or something that can uniquely
-// identify a user Here we will use: { user_id, email }; to allow both options
 fastifyPassport.registerUserSerializer(
     async (user: typeof usersSchema.$inferSelect, request) => {
-        return {
-            // this is the identifier, used in deserializer below
-            // TODO: define an interface for this
-            user_id: user.user_id,
-            email: user.email,
-        }
+        return { use_id: user.user_id };
     }
 );
 
-// turn you itentifier object into a user object.
 fastifyPassport.registerUserDeserializer(
-    async (identifier: any, request) => {
+    async (identifier: {user_id: number}, request) => {
         const resReadUser = await readUser(identifier.user_id);
         return (resReadUser.success) ? resReadUser.data : null;
     }
@@ -48,16 +42,7 @@ fastifyPassport.use(new LocalStrategy(
             return done(null, false);
         }
 
-        const userInfoForSession = {
-            user_id: resReadUser.data!.user_id,
-            name: resReadUser.data!.name,
-            email: resReadUser.data!.email,
-        };
-
-        // Note: userInfoForSession is what will get attached to `request.user` on successfull
-        // authentication. Therefore, it is important that we do not supply sensitive information
-        // TODO: Though I think `request.user` should not be stored in a session. So it should be fine
-        return done(null, userInfoForSession);
+        return done(null, resReadUser.data);
     }
 ));
 
@@ -74,13 +59,7 @@ fastifyPassport.use(new JwtStrategy(
             return done(null, false);
         }
 
-        const userInfoForSession = {
-            user_id: resReadUser.data!.user_id,
-            name: resReadUser.data!.name,
-            email: resReadUser.data!.email,
-        }
-
-        return done(null, userInfoForSession);
+        return done(null, resReadUser.data);
     }
 ));
 
