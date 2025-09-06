3c69d29a796beffef0d2287a43a1e2384549d69d
abdul <cseabdulrahim@gmail.com>
2025-09-06 15:44:14 +0530

    fix: make db.query.<table_name> available
    - currently, we do not register the schema in db initialization
    - the problem is that, now db.query API does not work, since it does not
      know what tables are available
    - we register the schema, in db initializetion and now queries like
    - db.query.users.findFirst() work

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,15 @@
-import mainConfig from "@src/configs/main.config.js";
+import mainConfig       from "@src/configs/main.config.js";
+import articlesSchema   from "@src/db/schemas/articles.schema.js";
+import tagsSchema       from "@src/db/schemas/tags.schema.js";
+import usersSchema      from "@src/db/schemas/users.schema.js";
 import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres";
 
-const db: NodePgDatabase = drizzle(mainConfig.DATABASE_URL);
+const schema = {
+    articles: articlesSchema,
+    tags: tagsSchema,
+    users: usersSchema,
+};
+
+const db: NodePgDatabase<typeof schema> = drizzle(mainConfig.DATABASE_URL, { schema  });
 
 export default db;
