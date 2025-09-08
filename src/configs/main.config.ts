645d165ac5ced7ecbb6a7d3c1cf017dd449e4721
abdul <cseabdulrahim@gmail.com>
2025-09-08 12:01:25 +0530

    style: indentations

--- src/configs/main.config.ts
+++ src/configs/main.config.ts
@@ -10,34 +10,34 @@ config({ processEnv: mainConfig });
 
 // define the interface/contract for mainConfig
 export interface IMainConfig {
-    POSTGRESQL_DATABASE:    string,
-    POSTGRESQL_USERNAME:    string,
-    POSTGRESQL_PASSWORD:    string,
-    DATABASE_URL:           string,
-    CUR_SERVER_PORT:        number,
-    CUR_SERVER_HOST:        string,
-    JWT_GENERATION_SECRET:  string,
-    JWT_EXPIRES_IN:         string,
-    SECURE_SESSION_KEY:     string,
-    SECURE_SESSION_EXPIRES_IN: number,
-    REDIS_PASSWORD:         string,
+    POSTGRESQL_DATABASE:        string,
+    POSTGRESQL_USERNAME:        string,
+    POSTGRESQL_PASSWORD:        string,
+    DATABASE_URL:               string,
+    CUR_SERVER_PORT:            number,
+    CUR_SERVER_HOST:            string,
+    JWT_GENERATION_SECRET:      string,
+    JWT_EXPIRES_IN:             string,
+    SECURE_SESSION_KEY:         string,
+    SECURE_SESSION_EXPIRES_IN:  number,
+    REDIS_PASSWORD:             string,
 }
 
 
 const mainConfigSchema: Schema = {
     type: "object",
     properties: {
-        POSTGRESQL_DATABASE:    { type: "string" },
-        POSTGRESQL_USERNAME:    { type: "string" },
-        POSTGRESQL_PASSWORD:    { type: "string" },
-        DATABASE_URL:           { type: "string" },
-        CUR_SERVER_PORT:        { type: "number", default: 7860 },
-        CUR_SERVER_HOST:        { type: "string", default: "0.0.0.0" },
-        JWT_GENERATION_SECRET:  { type: "string" },
-        JWT_EXPIRES_IN:         { type: "string" },
-        SECURE_SESSION_KEY:     { type: "string" },
-        SECURE_SESSION_EXPIRES_IN: { type: "number", default: 7 * 24 * 60 * 60 }, // 7 days
-        REDIS_PASSWORD:         { type: "string" }
+        POSTGRESQL_DATABASE:        { type: "string" },
+        POSTGRESQL_USERNAME:        { type: "string" },
+        POSTGRESQL_PASSWORD:        { type: "string" },
+        DATABASE_URL:               { type: "string" },
+        CUR_SERVER_PORT:            { type: "number", default: 7860 },
+        CUR_SERVER_HOST:            { type: "string", default: "0.0.0.0" },
+        JWT_GENERATION_SECRET:      { type: "string" },
+        JWT_EXPIRES_IN:             { type: "string" },
+        SECURE_SESSION_KEY:         { type: "string" },
+        SECURE_SESSION_EXPIRES_IN:  { type: "number", default: 7 * 24 * 60 * 60 }, // 7 days
+        REDIS_PASSWORD:             { type: "string" }
 
     },
     required: [
