13bad9713df1bc349779ee98e1872c68f23f2f6c
abdul <cseabdulrahim@gmail.com>
2025-09-08 19:46:10 +0530

    chore: remove unnecessary comments
    - these comments were added for my personal understanding and now that
      the logic is clear, they are no longer needded
    
    - also, we do not need th euesr to prof which user_id they are claiming.
      Randomly guessing a 32 byte stirng is extremly extremly difficult.

--- src/api/v1/refresh.ts
+++ src/api/v1/refresh.ts
@@ -1,21 +1,3 @@
-/**
- * Implementation of a refresh token mechanism. The /refresh endpoint, takes in a refresh token
- * in the request body. It validates the refresh token against the databse, and generates a new
- * pair of authentication token and refresh token.
- *
- * A refresh token is actually just long random string. It should be difficult to brute force.
- * In the database, we just use a key value store. (this should typically be done with a dedicated
- * session store like redis or levelDB), but we can start simple.
- *
- * We can then query the store as follows: map[refreshtoken] ->
- * if no result, refresh token is bogus or expired
- * if this returns a { email, id } attributes. Then the user is indeed genuine (as long as the key
- * is difficult to guess).
- *
- * We generate a new jwt token using hte data above, and also a new refresh token. We then overwrite
- * as: map[newrefreshtoken] = { email, id }. and delete map[oldrefreshtoken];
- * this ensures that old refresh token is invalidated automatically.
- **/
 import crypto           from "node:crypto";
 import redisMaps        from "@src/commons/redisMaps.js";
 import redis            from "@src/commons/redis.js";
@@ -35,12 +17,7 @@ const refreshEndpoint: FastifyPluginAsyncJsonSchemaToTs = async (app) => {
         { schema: refreshEndpointContract },
         async (request, response) => {
 
-            // try to find the user in the session store
             const oldRefreshToken = request.body.refresh_token;
-
-            // TODO, we should also ask this user what is the user_id he is claiming
-            // currently we just assume -> if he has the key -> userId for the key is his!
-            // he might be bruteforcing.
 
             const userId = await redis.hGet(redisMaps.refreshToken_userId, oldRefreshToken);
             if ( !userId ) {
