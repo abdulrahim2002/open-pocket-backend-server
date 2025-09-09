b3d518ac970220960e838f0f7cee78a3efb5b4f9
abdul <cseabdulrahim@gmail.com>
2025-09-09 12:24:20 +0530

    fix: setup plugin correctly, for types to work
    - setup the plugin which involves giving it type
      `FastifyPluginAsyncJsonSchemaToTs`
    - remove the typing from the app instance
    - and also make some style edits.

--- src/api/v1/add.ts
+++ src/api/v1/add.ts
@@ -2,29 +2,42 @@
  * /add endpoint
  * https://abdulrahim2002.github.io/open-pocket-backend-server/docs/API-spec/Endpoints/add/
  **/
-import type { FastifyInstance } from "fastify";
-import addRequestSchema         from "@src/api/v1/contracts/add.contract.js";
+import addRequestSchema from "@src/api/v1/contracts/add.contract.js";
+import { StatusCodes }  from "http-status-codes";
+import { FastifyPluginAsyncJsonSchemaToTs }
+                        from "@fastify/type-provider-json-schema-to-ts";
 
-async function addEndpoint(app: FastifyInstance) {
-    app.post('/add', { schema: addRequestSchema }, async (request, reply) => {
 
-        return {
-            item_id: "12345",
-            normal_url: "https://example.com/article",
-            resolved_id: "67890",
-            resolved_url: "https://example.com/resolved",
-            domain_id: "domain123",
-            origin_domain_id: "origin123",
-            response_code: "200",
-            mime_type: "text/html",
-            content_length: "1024",
-            encoding: "utf-8",
-            date_resolved: new Date().toISOString(),
-            date_published: new Date().toISOString(),
-            title: "Sample Article Title",
-            excerpt: "This is a sample excerpt of the article.",
-        };
-    });
-}
+const addEndpoint: FastifyPluginAsyncJsonSchemaToTs = async (app) => {
+
+    app.post(
+        "/add",
+        {
+            schema: addRequestSchema,
+        },
+        async (request, reply) => {
+            reply.status(StatusCodes.OK);
+            return {
+                item_id: "12345",
+                normal_url: "https://example.com/article",
+                resolved_id: "67890",
+                resolved_url: "https://example.com/resolved",
+                domain_id: "domain123",
+                origin_domain_id: "origin123",
+                response_code: "200",
+                mime_type: "text/html",
+                content_length: "1024",
+                encoding: "utf-8",
+                date_resolved: new Date().toISOString(),
+                date_published: new Date().toISOString(),
+                title: "Sample Article Title",
+                excerpt: "This is a sample excerpt of the article.",
+            }
+
+        }
+
+    );
+
+};
 
 export default addEndpoint;
