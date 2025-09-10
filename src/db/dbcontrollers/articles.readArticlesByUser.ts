bc61b21d8bb4d4b30dde15e58168d68298a5708e
abdul <cseabdulrahim@gmail.com>
2025-09-10 18:15:12 +0530

    feat: add support to fetch articels by user

---
+++ src/db/dbcontrollers/articles.readArticlesByUser.ts
@@ -0,0 +1,36 @@
+import articlesSchema   from "@src/db/schemas/articles.schema.js";
+import db               from "@src/db/index.js";
+import app              from "@src/app.js";
+import houndError       from "@src/db/dbcontrollers/commons/errorHounder.js";
+import { eq } from "drizzle-orm";
+import IDbControllerResponse, { OPSTATUS }
+                        from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
+import { StatusCodes } from "http-status-codes";
+
+
+type IArticle = typeof articlesSchema.$inferSelect;
+
+
+async function readArticlesByUser(user_id: number): Promise<IDbControllerResponse<IArticle[]>> {
+
+    try {
+        const fetchedArticles = await db.select().from(articlesSchema)
+                                        .where( eq(articlesSchema.user_id, user_id) );
+
+        app.log.info(`fetched articles for user: ${user_id}`);
+        return {
+            success: true,
+            status: OPSTATUS.SUCCESS,
+            message: `Articles retrieved for user: ${user_id}`,
+            recommendedHttpResponseCode: StatusCodes.OK,
+            data: fetchedArticles
+        }
+    }
+    catch (error: any) {
+        app.log.error(`Failed to articles for user. Details: ${error}`);
+        return houndError(error);
+    }
+
+}
+
+export default readArticlesByUser;
