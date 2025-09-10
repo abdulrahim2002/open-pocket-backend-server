1c23821fe24b78cff4b3dbfc17a171b197ba0fea
abdul <cseabdulrahim@gmail.com>
2025-09-10 14:49:11 +0530

    style: use 2 tab size for configs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 // see: https://orm.drizzle.team/docs/drizzle-config-file
 export default defineConfig({
-    out: './drizzle',
-    dialect: 'postgresql',
-    schema: './src/db/schemas',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
+  out: "./drizzle",
+  dialect: "postgresql",
+  schema: "./src/db/schemas",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 });
