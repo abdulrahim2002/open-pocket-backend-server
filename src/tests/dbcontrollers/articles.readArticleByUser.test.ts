9f82ba1c1dbe0d5fb668f24bcc9a0dcff76e18d7
abdul <cseabdulrahim@gmail.com>
2025-09-11 13:43:48 +0530

    feat: add test for readArticlesByUser

---
+++ src/tests/dbcontrollers/articles.readArticleByUser.test.ts
@@ -0,0 +1,67 @@
+import createArticle    from "@src/db/dbcontrollers/articles.createArticle.js";
+import deleteArticle    from "@src/db/dbcontrollers/articles.deleteArticle.js";
+import articlesSchema   from "@src/db/schemas/articles.schema.js";
+import createUser       from "@src/db/dbcontrollers/users.createUser.js";
+import deleteUser       from "@src/db/dbcontrollers/users.deleteUser.js";
+import usersSchema      from "@src/db/schemas/users.schema.js";
+import { OPSTATUS }     from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
+import { StatusCodes }  from "http-status-codes";
+import readArticlesByUser from "@src/db/dbcontrollers/articles.readArticlesByUser.js";
+
+
+const testUserForReadArticleByUser: typeof usersSchema.$inferInsert = {
+    provider: "open-pocket",
+    name: "test-user-read-article-user-by-user",
+    email: "test-user-read-article-by-user@mail.com",
+    hashed_password: "on124ho13ih482h9h2398hd289h993hr293g"
+};
+
+test("readArticleByUser", async () => {
+    // first create a test user
+    const resCreateUser = await createUser(testUserForReadArticleByUser);
+    if (!resCreateUser.success) {
+        throw new Error("Precondition Failure: Couldn't create a user in the first place");
+    }
+
+    // now create a new article belonging to test user created above
+    const testArticleForCreateArticle: typeof articlesSchema.$inferInsert = {
+        user_id: resCreateUser.data!.user_id, // very imp.
+        status: 0,
+        favorite: false,
+        given_url: "https://givenurl.com",
+        given_title: "Title supplied by the user",
+        resolved_title: "Sample Resolved Title",
+        resolved_url: "https://sample-resolved-url.com/",
+        domain_id: BigInt(213123),
+        origin_domain_id: BigInt(34242),
+        excerpt: "sample excerpt describing the item in detail",
+        is_article: true,
+        is_index: false,
+        has_video: 0,
+        has_image: 0,
+        word_count: 123,
+        time_added: new Date(),
+        time_updated: new Date(),
+        top_image_url: "https://topimage.io/image.png/",
+        author_id: 12,
+    };
+
+    const resCreateArticle = await createArticle(testArticleForCreateArticle);
+
+    // now find the article using user_id
+    const resReadArticlesByUser = await readArticlesByUser(resCreateUser.data!.user_id);
+
+    // delete the article to reverse changes made
+    await deleteArticle(resCreateArticle.data!.item_id);
+
+    // delete the test user as well
+    await deleteUser(resCreateUser.data!.user_id);
+
+    expect(resReadArticlesByUser).toMatchObject({
+        success: true,
+        status: OPSTATUS.SUCCESS,
+        message: `Articles retrieved for user: ${resCreateUser.data!.user_id}`,
+        recommendedHttpResponseCode: StatusCodes.OK,
+        data: resReadArticlesByUser.data,
+    });
+});
