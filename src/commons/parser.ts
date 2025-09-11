c5061d6b30b852c175e9e73379b8943af2ebfd99
abdul <cseabdulrahim@gmail.com>
2025-09-11 22:17:43 +0530

    fix: add extended interface for parser response
    - the parser may not always return the required data, especially when an
      error occurs
    - therefore, add error based fields, and a data field. The idea is, when
      an error occurs, return as much information about it as possible

--- src/commons/parser.ts
+++ src/commons/parser.ts
@@ -6,40 +6,59 @@
  * If the original article redirects. For example, say it was
  * bit.ly/qdsq3 then the parser shall find out the final resolved url.
 */
-
 export interface IParserResponse {
-    resolved_url: string,
-    resolved_title: string,
-    excerpt: string,
-    word_count: number,
-    has_image: number,
-    has_video: number,
-    is_index: boolean,
-    is_article: boolean,
-    top_image_url: string,
-
-    // metadata
-    mime_type: string,
-    content_length: string,
-    encoding: string,
+    success: boolean,
+    status: number,
+    message: string,
+    data: {
+        // final URL after following redirect
+        resolved_url: string,
+        // automatically found title in resolved_url article
+        resolved_title: string,
+        // excerpet if found
+        excerpt: string,
+        // numebr of words in the article
+        word_count: number,
+        // weather the article has image
+        has_image: number,
+        // weather the article has video
+        has_video: number,
+        // weather the page is an index page
+        is_index: boolean,
+        // weather the page is an article
+        is_article: boolean,
+        // URL of the first image found in the page body (article) if any
+        top_image_url: string,
+        // mime type of the page
+        mime_type: string,
+        // content length of the page
+        content_length: string,
+        // encoding of the page
+        encoding: string,
+    }
 };
 
 async function parser(url: string): Promise<IParserResponse> {
 
     return {
-        resolved_url: url,
-        resolved_title: "WIP",
-        excerpt: "WIP",
-        word_count: 0,
-        has_image: 0,
-        has_video: 0,
-        is_index: false,
-        is_article: false,
-        top_image_url: "WIP",
+        success: true,
+        status: 0,
+        message: "",
+        data: {
+            resolved_url: url,
+            resolved_title: "WIP",
+            excerpt: "WIP",
+            word_count: 0,
+            has_image: 0,
+            has_video: 0,
+            is_index: false,
+            is_article: false,
+            top_image_url: "WIP",
 
-        mime_type:      "Needs backend parser, schema upgrade | WIP",
-        content_length: "Needs backend parser, schema upgrade | WIP",
-        encoding:       "Needs backend parser, schema upgrade | WIP",
+            mime_type:      "Needs backend parser, schema upgrade | WIP",
+            content_length: "Needs backend parser, schema upgrade | WIP",
+            encoding:       "Needs backend parser, schema upgrade | WIP",
+        }
     }
 }
 
