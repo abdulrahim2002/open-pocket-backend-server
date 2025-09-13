418a993143551eaacceed193651b82780955dbcb
abdul <cseabdulrahim@gmail.com>
2025-09-13 23:14:27 +0530

    fix: add support for typing information
    - if we need typign inforamtion on request.body and response
    - we need to type the plugin as `FastifyPluginAsyncJsonSchemaToTs`

--- src/api/v1/send.ts
+++ src/api/v1/send.ts
@@ -1,12 +1,15 @@
-import { type FastifyInstance } from "fastify";
-import fastifyPassport          from "@src/commons/fastifyPassport.js";
-import sendEndpointContract     from "@src/api/v1/contracts/send.contract.js";
-
 /**
  * /send endpoint
  * https://abdulrahim2002.github.io/open-pocket-docs/docs/API-spec/Endpoints/send/
  **/
-async function sendEndpoint( app: FastifyInstance ) {
+import fastifyPassport          from "@src/commons/fastifyPassport.js";
+import sendEndpointContract     from "@src/api/v1/contracts/send.contract.js";
+import { FastifyPluginAsyncJsonSchemaToTs }
+                                from "@fastify/type-provider-json-schema-to-ts";
+
+
+const sendEndpoint: FastifyPluginAsyncJsonSchemaToTs = async (app) => {
+
     app.post(
         "/send",
         {
@@ -14,13 +17,15 @@ async function sendEndpoint( app: FastifyInstance ) {
             preValidation: fastifyPassport.authenticate(["secure-session", "jwt"])
         },
         async (request, reply) => {
-
-        // send a dummy response
-        return {
-            status: 1,
-            action_results: [true, false, true]
+            // send a dummy response
+            return {
+                status: 1,
+                action_results: [true, false, true]
+            }
         }
-    })
-}
+
+    );
+
+};
 
 export default sendEndpoint;
