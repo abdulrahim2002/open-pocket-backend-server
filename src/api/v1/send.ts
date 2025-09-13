71273a9ab7236fc1e5170949234574118b054675
abdul <cseabdulrahim@gmail.com>
2025-09-13 23:31:41 +0530

    feat: implement basic support for `/send` endpoint
    - implement support for the add action
    - the endpoint iterates through each action. and for each unsupported
      action, it silengtly appends false to the result.
    - else it makes the relevant calls and performs the action

--- src/api/v1/send.ts
+++ src/api/v1/send.ts
@@ -4,8 +4,10 @@
  **/
 import fastifyPassport          from "@src/commons/fastifyPassport.js";
 import sendEndpointContract     from "@src/api/v1/contracts/send.contract.js";
+import actionMap                from "@src/api/v1/actions/index.js";
 import { FastifyPluginAsyncJsonSchemaToTs }
                                 from "@fastify/type-provider-json-schema-to-ts";
+import { StatusCodes } from "http-status-codes";
 
 
 const sendEndpoint: FastifyPluginAsyncJsonSchemaToTs = async (app) => {
@@ -16,12 +18,33 @@ const sendEndpoint: FastifyPluginAsyncJsonSchemaToTs = async (app) => {
             schema: sendEndpointContract,
             preValidation: fastifyPassport.authenticate(["secure-session", "jwt"])
         },
-        async (request, reply) => {
-            // send a dummy response
-            return {
-                status: 1,
-                action_results: [true, false, true]
+        async (request, response) => {
+            let status = 1;
+            const action_results = new Array<boolean>();
+
+            for (const actionObj of request.body.actions) {
+                // result of current action
+                let resCurAction;
+
+                if (!actionMap[actionObj.action]) {
+                    // if action string not in map -> ignore silently
+                    app.log.warn(`send.endpoint: unknown action '${actionObj.action}'`);
+                    resCurAction = false;
+                }
+                else {
+                    // pass the whole actionObject. Unnecessary fields will be ignored!
+                    resCurAction = await actionMap[actionObj.action]!(actionObj);
+                }
+
+                action_results.push(resCurAction);
+                if (!resCurAction) status = 0;
             }
+
+            response.status(StatusCodes.OK);
+            return {
+                status,
+                action_results,
+            };
         }
 
     );
