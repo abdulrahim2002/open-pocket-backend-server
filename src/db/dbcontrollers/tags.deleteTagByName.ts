98dcf3e1da6f5f8d2dd3119efdc41bd5a12c61c9
abdul <cseabdulrahim@gmail.com>
2025-09-14 20:26:46 +0530

    feat: add support for deleting tags (multiple)
    - add support for deleting multiple tags by name
    - we need to supply the `item_id` and `user_id` to pinpoint the exact
      article to operate on (and with access rights)
    - only then, we can allow user to supply a list of tags they want to
      delete from that article

---
+++ src/db/dbcontrollers/tags.deleteTagByName.ts
@@ -0,0 +1,46 @@
+/**
+ * @description Delete all tags which match the given `user_id` and `item_id` along
+ * with `tag_name` in provided list of tags
+ */
+import app              from "@src/app.js";
+import db               from "@src/db/index.js";
+import tagsSchema       from "@src/db/schemas/tags.schema.js";
+import { StatusCodes }  from "http-status-codes";
+import houndError       from "@src/db/dbcontrollers/commons/errorHounder.js";
+import IDbControllerResponse, { OPSTATUS }
+                        from "@src/db/dbcontrollers/commons/IDbControllerResponse.js";
+import { eq, and, inArray }     from "drizzle-orm";
+
+type tagShape = typeof tagsSchema.$inferSelect;
+
+async function deleteTagsByName(item_id: bigint, user_id: number, tags: string[]):
+                Promise<IDbControllerResponse<tagShape[]>> {
+
+    try {
+        const deletedTags = await db.delete(tagsSchema)
+                                    .where(
+                                        and(
+                                            eq(tagsSchema.item_id, item_id),
+                                            eq(tagsSchema.user_id, user_id),
+                                            inArray(tagsSchema.tag_name, tags)
+                                        )
+                                    )
+                                    .returning();
+
+        app.log.info(`${deletedTags.length} tags deleted successfully`);
+
+        return {
+            success: true,
+            status: OPSTATUS.SUCCESS,
+            message: "tags deleted successfully",
+            recommendedHttpResponseCode: StatusCodes.OK,
+            data: deletedTags,
+        }
+    }
+    catch (error: any) {
+        app.log.error(error);
+        return houndError(error);
+    }
+}
+
+export default deleteTagsByName;
