ede968286409532235a7bf9404302de3c5a50967
abdul <cseabdulrahim@gmail.com>
2025-09-14 18:33:00 +0530

    feat: add unfavorite action in send endpoint
    - add support for unfavorite action

--- src/api/v1/actions/index.ts
+++ src/api/v1/actions/index.ts
@@ -2,12 +2,14 @@ import addAction        from "@src/api/v1/actions/add.action.js";
 import archiveAction    from "@src/api/v1/actions/archive.action.js";
 import readdAction      from "@src/api/v1/actions/readd.action.js";
 import favoriteAction   from "@src/api/v1/actions/favorite.action.js";
+import unFavoriteAction from "@src/api/v1/actions/unfavorite.action.js";
 
 const actionMap: { [key: string]: Function } = {
     "add":      addAction,
     "archive":  archiveAction,
     "readd":    readdAction,
     "favorite": favoriteAction,
+    "unfavorite": unFavoriteAction,
 };
 
 export default actionMap;
