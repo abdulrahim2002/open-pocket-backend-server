b021659ef926e9bc308054055b3f3795735d5020
abdul <cseabdulrahim@gmail.com>
2025-09-14 13:28:21 +0530

    fix: actions are strings (make it explicit)
    - by default, when you define
    
    ```
    const dict = {
    	name: "abdul"
    }
    ```
    
    then you can do:
    ```
    dict["name"] // returns "abdul"
    ```
    
    But still, we should make it clear in code that input actions (strings)
    are converted into their handler functionss

--- src/api/v1/actions/index.ts
+++ src/api/v1/actions/index.ts
@@ -2,8 +2,8 @@ import addAction    from "@src/api/v1/actions/add.action.js";
 import archiveAction from "@src/api/v1/actions/archive.action.js";
 
 const actionMap: { [key: string]: Function } = {
-    add: addAction,
-    archive: archiveAction,
+    "add": addAction,
+    "archive": archiveAction,
 };
 
 export default actionMap;
