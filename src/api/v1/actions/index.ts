af7922b813ffafcd54914459c9f9d9e6e7d5f4b5
abdul <cseabdulrahim@gmail.com>
2025-09-14 17:52:32 +0530

    feat: implement readd action
    - the readd action basically unarchives an article by settings `status=0`
    - it is same as reversing the archive or unarchive
    - documenation: https://abdulrahim2002.github.io/open-pocket-docs/docs/API-spec/Endpoints/send/#action-readd

--- src/api/v1/actions/index.ts
+++ src/api/v1/actions/index.ts
@@ -1,9 +1,11 @@
 import addAction    from "@src/api/v1/actions/add.action.js";
 import archiveAction from "@src/api/v1/actions/archive.action.js";
+import readdAction      from "@src/api/v1/actions/readd.action.js";
 
 const actionMap: { [key: string]: Function } = {
     "add": addAction,
     "archive": archiveAction,
+    "readd":    readdAction
 };
 
 export default actionMap;
---
+++ src/api/v1/actions/readd.action.ts
@@ -0,0 +1,62 @@
+/**
+ * @description Unarchive an article by setting it's status to 0
+ * see: https://abdulrahim2002.github.io/open-pocket-docs/docs/API-spec/Endpoints/send/#action-readd
+ */
+import updateArticle    from "@src/db/dbcontrollers/article.updateArticle.js";
+import { type Schema }  from "ajv";
+import app              from "@src/app.js";
+import ajv              from "@src/commons/ajv.js";
+
+
+interface IReaddAction {
+    item_id:    string,
+};
+
+
+const readdActionSchema: Schema = {
+    type: "object",
+    properties: {
+        // bigint range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        // see: https://www.postgresql.org/docs/current/datatype-numeric.html
+        item_id: { type: "string", pattern: "^\\d+$", minLength: 1, maxLength: 20 }
+    },
+    required: ["item_id"]
+};
+
+
+async function readdAction( data: IReaddAction ): Promise<boolean> {
+
+    try {
+
+        if ( !ajv.validate(readdActionSchema, data) ) {
+            throw new Error("Schema validation failed" + ajv.errorsText());
+        }
+
+        const item_id = BigInt(data.item_id);
+        if (item_id <= 0n || item_id > 9223372036854775807n) {
+            throw new Error("item_id is out of range");
+        }
+
+        const resUpdate = await updateArticle(item_id, {
+            // https://abdulrahim2002.github.io/open-pocket-docs/docs/Database-Layer/database-schema/#description-of-the-fields
+            status: 0
+        });
+
+        if (resUpdate.success) {
+            app.log.info(`Article with ID ${item_id} readded successfully.`);
+            return true;
+        }
+
+        app.log.error(`Failed to readd article with ID ${item_id}: ${resUpdate.message}`);
+        return false;
+
+    }
+
+    catch (error: any) {
+        // TODO: debug error properly
+        app.log.error(error);
+        return false;
+    }
+}
+
+export default readdAction;
