84a5182c807f0556fea5a3eb29bb33517cb0b336
abdul <cseabdulrahim@gmail.com>
2025-09-14 17:53:31 +0530

    style: fix indentation issues

--- src/api/v1/actions/index.ts
+++ src/api/v1/actions/index.ts
@@ -1,10 +1,10 @@
-import addAction    from "@src/api/v1/actions/add.action.js";
-import archiveAction from "@src/api/v1/actions/archive.action.js";
+import addAction        from "@src/api/v1/actions/add.action.js";
+import archiveAction    from "@src/api/v1/actions/archive.action.js";
 import readdAction      from "@src/api/v1/actions/readd.action.js";
 
 const actionMap: { [key: string]: Function } = {
-    "add": addAction,
-    "archive": archiveAction,
+    "add":      addAction,
+    "archive":  archiveAction,
     "readd":    readdAction
 };
 
