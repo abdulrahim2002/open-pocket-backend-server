754998162c86945b406ef57bfef301b1844eb77a
abdul <cseabdulrahim@gmail.com>
2025-09-14 13:27:49 +0530

    fix: export archive action

--- src/api/v1/actions/index.ts
+++ src/api/v1/actions/index.ts
@@ -1,7 +1,9 @@
-import addAction from "@src/api/v1/actions/add.action.js";
+import addAction    from "@src/api/v1/actions/add.action.js";
+import archiveAction from "@src/api/v1/actions/archive.action.js";
 
 const actionMap: { [key: string]: Function } = {
     add: addAction,
+    archive: archiveAction,
 };
 
 export default actionMap;
