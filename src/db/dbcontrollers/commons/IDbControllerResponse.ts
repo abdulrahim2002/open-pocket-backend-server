de2d3977944a15ac17ae79ee6aaa6a1271e0a24e
abdul <cseabdulrahim@gmail.com>
2025-09-15 17:07:18 +0530

    chore: move OPSTATUS in commons

--- src/db/dbcontrollers/commons/IDbControllerResponse.ts
+++ src/db/dbcontrollers/commons/IDbControllerResponse.ts
@@ -1,25 +1,3 @@
-// Relevant codes from: https://www.postgresql.org/docs/current/errcodes-appendix.html
-export enum OPSTATUS {
-    SUCCESS,
-    // integrity violations
-    FOREIGN_KEY_VIOLATION=23503,
-    UNIQUE_VIOLATION=23505,
-    CHECK_VIOLATION=23514,
-    NOT_NULL_VIOLATION=23502,
-
-    // transaction failure
-    INVALID_TRANSACTION_STATE=25000,
-
-    // connection failure
-    CONNECTION_FAILURE=8006,
-
-    // custom errrors
-    USER_DOES_NOT_EXISTS=7860,
-
-    // other
-    UNKNOWN_FAILURE=-1,
-}
-
 interface IDbControllerResponse<T=any> {
     // true if operation was successfull
     success: boolean,
