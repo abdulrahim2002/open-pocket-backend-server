455418e258662dc1e8a58044f73f9dea4c9f5a1c
abdul <cseabdulrahim@gmail.com>
2025-09-16 20:27:59 +0530

    chore: add formats information in ajv instance

--- src/commons/ajv.ts
+++ src/commons/ajv.ts
@@ -3,10 +3,13 @@
  * see: https://ajv.js.org/api.html#new-ajv-options-object
  **/
 import { Ajv } from "ajv";
+import formatsPlugin from "ajv-formats";
 
 const ajv = new Ajv({
     useDefaults: true,
     coerceTypes: true
 });
+
+formatsPlugin.default(ajv);
 
 export default ajv;
